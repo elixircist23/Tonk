29c9a95bde40d5174615a5be0f93499680d34a82
tbone255 <taha.amir13971@gmail.com>
2014-12-02 17:11:53 -0500

    Card.java
    Card Class

--- Card.java
+++ Card.java
@@ -1,3 +1,8 @@
-public class Card{
+public class Card
+{
+
+  //Add a arraylist of cards in the deck called downDeck <-- (the facedown deck)
+  //Add a arraylist of cards in the deck called upDeck <-- (the faceup discard deck)
+
 
 }
