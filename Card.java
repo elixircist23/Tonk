45b4a6b3bc63c214505ce126ab31a1a7db1c01d0
elixircist23 <elixircist@gmail.com>
2014-12-05 22:34:55 -0500

    Update Card.java

--- Card.java
+++ Card.java
@@ -1,16 +1,77 @@
+//Importing all HashMap libraries
+import java.util.*;
 
-public class Tonk {
-
-	public static void main(String[] args) {
+public class Card {
+	
+	//Creating HashMap with String Keys and Integer Values
+	public HashMap<String,Integer> deck = new HashMap<String, Integer>();
+	
+	public Card(){
 		
-		Card deck = new Card();
-		String random = deck.randomCard();
-		int valueOfRandom = deck.getValue(random);
-		System.out.println(random);
-		System.out.println(valueOfRandom);
+		//Creating all the cards and placing them in the HashMap called 'deck'
+		for(int i = 2; i < 10; i++){
+			deck.put(i + " of Hearts", i);
+		}
+		
+		for(int i = 2; i < 10; i++){
+			deck.put(i + " of Spades", i);
+		}
+		
+		for(int i = 2; i < 10; i++){
+			deck.put(i + " of Clubs", i);
+		}
+		
+		for(int i = 2; i < 10; i++){
+			deck.put(i + " of Diamonds", i);
+		}
+		
+		deck.put("Ace of Hearts", 10);
+		deck.put("Ace of Spades", 10);
+		deck.put("Ace of Clubs", 10);
+		deck.put("Ace of Diamonds", 10);
+		
+		deck.put("King of Hearts", 10);
+		deck.put("King of Spades", 10);
+		deck.put("King of Clubs", 10);
+		deck.put("King of Diamonds", 10);
+		
+		deck.put("Queen of Hearts", 10);
+		deck.put("Queen of Spades", 10);
+		deck.put("Queen of Clubs", 10);
+		deck.put("Queen of Diamonds", 10);
+		
+		deck.put("Jack of Hearts", 10);
+		deck.put("Jack of Spades", 10);
+		deck.put("Jack of Clubs", 10);
+		deck.put("Jack of Diamonds", 10);
 		
 		
-
 	}
-
+	
+	public void print(){
+		System.out.println(deck);
+	}
+	
+	public String randomCard(){
+		
+		//Selects a random card from deck
+		//Gets value and Key, but returns a String of the Key
+		Random random = new Random();
+		List<String> keys = new ArrayList<String>(deck.keySet());
+		String randomKey = keys.get(random.nextInt(keys.size()));
+		Integer value = deck.get(randomKey);
+		
+		return randomKey;
+		
+	}
+	
+	public int getValue(String key){
+		//returns the value of the card, given the key
+		int value = deck.get(key);
+		return value;
+	}
+	
+	
+	
+	
 }
