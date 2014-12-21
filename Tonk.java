7c055262b42755c55536bf14403204700dea2d39
elixircist23 <elixircist@gmail.com>
2014-12-21 13:55:38 -0500

    Update Tonk.java

--- Tonk.java
+++ Tonk.java
@@ -1,8 +1,26 @@
-
+import java.util.*;
 public class Tonk {
 
 	public static void main(String[] args) {
-    		System.out.println("hello");
+		
+		
+		Card deck = new Card();
+		String[] test;
+		test = deck.deal();
+		System.out.println(Arrays.toString(test));
+		
+		
+		
+		
+		
+		
+		/*
+		int valueOfRandom = deck.getValue(random);
+		System.out.println(valueOfRandom);
+		*/
+		
+		
+		
 
 	}
 
