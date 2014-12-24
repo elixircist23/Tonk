f0113bbe6bfaf3c7d05a8394d465d25ddd897e29
elixircist23 <elixircist@gmail.com>
2014-12-23 20:52:57 -0500

    setting up the player class

---
+++ Player.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Player {
+	
+	//Grab the value of the used cards from the card class itself
+	ArrayList<String> USED = Card.USED;
+	
+	public Player(){
+		
+		System.out.println(USED);
+		
+	}
+	
+	
+
+}
